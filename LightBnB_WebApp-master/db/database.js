779f7c802413c771cf82a7c61361d80a373df2e8
Kade Kobussen <kadekobussen50@gmail.com>
2023-04-06 16:01:36 -0700

    refactor getALLProperties

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -76,27 +76,10 @@ const addUser = function (user) {
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
- * @param {number} limit The number of results to return (default: 10).
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool
-    .query(`
-      SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
-      FROM reservations
-        JOIN properties ON reservations.property_id = properties.id
-        JOIN property_reviews ON properties.id = property_reviews.property_id
-      WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
-      GROUP BY reservations.id, properties.id
-      ORDER BY reservations.start_date
-      LIMIT $2;
-    `, [guest_id, limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  return getAllProperties(null, 2);
 };
 
 /// Properties
@@ -107,17 +90,49 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAllProperties = function (options, limit = 10) {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  
+  let whereClause = "";
+  let counter = 0;
+  if (options.owner_id) {
+    counter++;
+    queryParams.push(options.owner_id);
+    whereClause += `owner_id = $${counter}`;
+  }
+  if (options.minimum_price_per_night) {
+    counter++;
+    queryParams.push(options.minimum_price_per_night * 100);
+    whereClause += `${counter === 1 ? "" : " AND "}cost_per_night >= $${counter}`;
+  }
+  if (options.maximum_price_per_night) {
+    counter++;
+    queryParams.push(options.maximum_price_per_night * 100);
+    whereClause += `${counter === 1 ? "" : " AND "}cost_per_night <= $${counter}`;
+  }
+  if (options.minimum_rating) {
+    counter++;
+    queryParams.push(options.minimum_rating);
+    whereClause += `${counter === 1 ? "" : " AND "}property_reviews.rating >= $${counter}`;
+  }
+  if (whereClause.length > 0) {
+    queryString += `WHERE ${whereClause} `;
+  }
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  console.log(queryString, queryParams);
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
 
 /**
  * Add a property to the database
